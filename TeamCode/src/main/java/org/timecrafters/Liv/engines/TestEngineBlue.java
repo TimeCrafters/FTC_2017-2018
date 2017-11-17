259b9fd475e0a57120ca13da651ba509b8b375b3
Liv007 <Olivia55045@gmail.com>
2017-11-16 20:03:31 -0600

    I made a robot drive forward and turn

--- TeamCode/src/main/java/org/timecrafters/Liv/engines/TestEngineBlue.java
+++ TeamCode/src/main/java/org/timecrafters/Liv/engines/TestEngineBlue.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.timecrafters.engine.Engine;
 import org.timecrafters.gfp.state.drive.DriveStraightForward;
+import org.timecrafters.gfp.state.drive.TurnLeft;
 
 /**
  * Created by Liv on 11/14/2017.
@@ -14,5 +15,10 @@ public class TestEngineBlue extends Engine {
     @Override
     public void setProcesses() {
         addState(new DriveStraightForward(this,0.4,1500));
+        addState(new TurnLeft(this,0.3,1000));
     }
+
+
+
 }
+
