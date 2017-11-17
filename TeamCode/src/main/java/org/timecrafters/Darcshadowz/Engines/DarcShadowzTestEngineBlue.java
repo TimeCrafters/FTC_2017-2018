ad5cd438581e45d5082cb3250dda493e2bacde09
Darcshadowz <31678192+Darcshadowz@users.noreply.github.com>
2017-11-16 19:57:05 -0600

    added a turn to engine

--- TeamCode/src/main/java/org/timecrafters/Darcshadowz/Engines/DarcShadowzTestEngineBlue.java
+++ TeamCode/src/main/java/org/timecrafters/Darcshadowz/Engines/DarcShadowzTestEngineBlue.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.timecrafters.engine.Engine;
 import org.timecrafters.gfp.state.drive.DriveStraightForward;
+import org.timecrafters.gfp.state.drive.TurnLeft;
 
 /**
  * Created by Dylan on 11/14/2017.
@@ -16,8 +17,7 @@ public class DarcShadowzTestEngineBlue extends Engine {
 
     public void setProcesses() {
         addState(new DriveStraightForward(this, .3, 1500));
-
-
+        addState(new TurnLeft(this, .3, 500));
 
 
 
