9f66883527102dc7e59eeb50ed91666831515bb8
GoldfishPi <erikbadger777@gmail.com>
2017-11-25 11:05:26 -0600

    Removed broken class

--- TeamCode/src/main/java/org/timecrafters/gfp/engines/TestEngine.java
+++ TeamCode/src/main/java/org/timecrafters/gfp/engines/TestEngine.java
@@ -3,9 +3,7 @@ package org.timecrafters.gfp.engines;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.timecrafters.engine.Engine;
-import org.timecrafters.engine.UnstableEngine;
-import org.timecrafters.gfp.state.TestState;
-import org.timecrafters.gfp.temp.ButtClass;
+
 
 /**
  * Created by t420 on 9/14/2017.
@@ -15,7 +13,7 @@ public class TestEngine extends Engine {
 
     @Override
     public void setProcesses(){
-        addState(new ButtClass(this));
+        //addState(new ButtClass(this));
         //addState(new DriveTest(this));
     }
 
