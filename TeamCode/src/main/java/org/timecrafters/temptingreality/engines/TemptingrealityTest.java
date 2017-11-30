07a408e7228781c9b6c2639c0d16181a7e95c5bb
temptingreality <lukebadger777@gmail.com>
2017-11-30 17:44:52 -0600

    reversed derection on dc motor front left and back left

--- TeamCode/src/main/java/org/timecrafters/temptingreality/engines/TemptingrealityTest.java
+++ TeamCode/src/main/java/org/timecrafters/temptingreality/engines/TemptingrealityTest.java
@@ -3,6 +3,8 @@ package org.timecrafters.temptingreality.engines;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.timecrafters.engine.Engine;
+import org.timecrafters.gfp.state.Arm.ExtendArm;
+import org.timecrafters.gfp.state.Arm.RaiseArm;
 import org.timecrafters.gfp.state.drive.DriveStraightForward;
 
 /**
@@ -12,7 +14,11 @@ import org.timecrafters.gfp.state.drive.DriveStraightForward;
 public class TemptingrealityTest extends Engine {
     @Override
     public void setProcesses() {
-        addState(new DriveStraightForward(this,.5,100000));
+        addState(new DriveStraightForward(this,.5,100));
+
+        addState(new RaiseArm(this,1,400));
+
+        addState(new ExtendArm(this,1,600));
 
     }
 }
