163c1debc8fdd101e1ee8520332a62e1814f7b4f
GoldfishPi <erikbadger777@gmail.com>
2017-11-30 20:36:13 -0600

    Added ReadRightSensor

---
+++ TeamCode/src/main/java/org/timecrafters/gfp/state/ultrasonic/ReadRightSensor.java
@@ -0,0 +1,17 @@
+package org.timecrafters.gfp.state.ultrasonic;
+
+import org.timecrafters.engine.Engine;
+
+/**
+ * Created by goldfishpi on 11/30/17.
+ */
+
+public class ReadRightSensor extends UltraSonic {
+    public ReadRightSensor(Engine engine, int readings) {
+        super(engine, readings);
+    }
+    public void init(){
+        super.init();
+        setSensor(frontRightDistanceSensor);
+    }
+}
