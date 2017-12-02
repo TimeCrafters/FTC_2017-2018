f4a2a4104c733f41b1fb6f373466b6152cdb8064
Darcshadowz <31678192+Darcshadowz@users.noreply.github.com>
2017-12-02 12:31:28 -0600

    Modified engine to place glyph in leftmost collumn

--- TeamCode/src/main/java/org/timecrafters/Darcshadowz/Engines/DarcShadowzTestEngineBlue.java
+++ TeamCode/src/main/java/org/timecrafters/Darcshadowz/Engines/DarcShadowzTestEngineBlue.java
@@ -4,7 +4,12 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.timecrafters.Darcshadowz.State.VeeringRight;
 import org.timecrafters.engine.Engine;
+import org.timecrafters.gfp.state.Arm.ExtendArm;
+import org.timecrafters.gfp.state.Arm.RaiseArm;
 import org.timecrafters.gfp.state.drive.DriveStraightForward;
+import org.timecrafters.gfp.state.drive.TurnLeft;
+import org.timecrafters.gfp.state.drive.TurnRight;
+import org.timecrafters.gfp.state.grabber.LeftGrabber;
 
 /**
  * Created by Dylan on 11/14/2017.
@@ -17,15 +22,16 @@ public class DarcShadowzTestEngineBlue extends Engine {
 
     public void setProcesses() {
 
-  //      addState(new VeeringRight(this));
-     addState(new DriveStraightForward(this, .1, 1000));
-//        addState(new RaiseArm(this, 1, 500));
-//        addState(new ExtendArm(this, 1, 1550));
-//        addState(new LeftGrabber(this, 0.5, 150));
-//        addState(new DriveStraightBackward(this, .2, 500));
-//        addState(new TurnLeft(this, .3, 750));
-//        addState(new RaiseArm(this, -0.5, 500));
-//        addState(new DriveStraightForward(this, .3, 1000));
+          addState(new DriveStraightForward(this, .1, 2200));
+          //addState(new ExtendArm(this, 1, 1000));
+         // addState(new RaiseArm(this, 1, 2500));
+          addState(new TurnLeft(this, .2, 1025));
+          addState(new DriveStraightForward(this, 0.2, 1300));
+          addState(new TurnRight(this,0.2,1025));
+          addState(new DriveStraightForward(this, 0.1, 500));
+          addState(new ExtendArm(this, 1, 1700));
+   //       addState(new RaiseArm(this, -1, 1750));
+          addState(new LeftGrabber(this, 0.5, 500));
 
 
     }
---
+++ TeamCode/src/main/java/org/timecrafters/Darcshadowz/State/ReadingColor.java
@@ -0,0 +1,22 @@
+package org.timecrafters.Darcshadowz.State;
+
+import org.timecrafters.engine.Engine;
+import org.timecrafters.gfp.config.Config;
+
+/**
+ * Created by Dylan on 12/2/2017.
+ */
+
+public class ReadingColor extends Config{
+
+
+    public ReadingColor(Engine engine) {
+        super(engine);
+
+
+
+
+
+
+    }
+}
