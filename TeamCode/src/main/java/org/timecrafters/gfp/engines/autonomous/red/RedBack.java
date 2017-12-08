1db39df87b82bdb97852af66c57f0d2a010568ad
Liv007 <Olivia55045@gmail.com>
2017-12-07 20:06:52 -0600

    Back Red autonomous mode drive strait forward, and turn right.

--- TeamCode/src/main/java/org/timecrafters/gfp/engines/autonomous/red/RedBack.java
+++ TeamCode/src/main/java/org/timecrafters/gfp/engines/autonomous/red/RedBack.java
@@ -15,7 +15,7 @@ import org.timecrafters.gfp.state.drive.TurnRight;
 public class RedBack extends Engine{
     @Override
     public void setProcesses() {
-        addState(new DriveStraightForward(this, 0.2, 4171 ));
+        addState(new DriveStraightForward(this, 0.2, 4172 ));
         addState(new TurnRight(this, 0.2, 1537));
         addState(new DriveStraightForward(this, 0.2, 2000)
 
