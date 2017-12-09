d041e876b727dd37d450d95aa960700f9d9d99c4
Liv007 <Olivia55045@gmail.com>
2017-12-09 12:27:11 -0600

    fixed merge errors

--- TeamCode/src/main/java/org/timecrafters/Liv/engines/RedBack.java
+++ TeamCode/src/main/java/org/timecrafters/Liv/engines/RedBack.java
@@ -15,9 +15,9 @@ import org.timecrafters.gfp.state.grabber.LeftGrabber;
 public class RedBack extends Engine{
     @Override
     public void setProcesses() {
-        addState(new DriveStraightForward(this, 0.2, 4558 ));
+        addState(new DriveStraightForward(this, 0.2, 4758 ));
         addState(new TurnRight(this, 0.2, 1281));
-        addState(new RaiseArm(this, 0.5, 215));
+        addState(new RaiseArm(this, 0.5, 430));
         addState(new ExtendArm(this, 0.5, 1290));
         addState(new LeftGrabber(this, 0.5, 215));
         addState(new DriveStraightForward(this, 0.5, -860));
