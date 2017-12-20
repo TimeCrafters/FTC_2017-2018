ad16640dc9c72e8e2392dfc6201ad37d36fcb308
Liv007 <Olivia55045@gmail.com>
2017-12-19 19:32:01 -0600

    changes

--- TeamCode/src/main/java/org/timecrafters/Liv/engines/RedBack.java
+++ TeamCode/src/main/java/org/timecrafters/Liv/engines/RedBack.java
@@ -17,7 +17,7 @@ import org.timecrafters.gfp.state.grabber.LeftGrabber;
 public class RedBack extends Engine{
     @Override
     public void setProcesses() {
-        switch (2) {
+        switch (3) {
             case 1:
                 // center glyph goal (WORKING!!!!!)
                 addState(new DriveStraightForward(this, 0.3, 4730));
@@ -32,7 +32,7 @@ public class RedBack extends Engine{
                 addState(new DriveStraightForward(this, 0.5, 860));
                 break;
             case 2:
-                //Right glyph goal
+                //Right glyph goal (WORKING!!!!!)
                 addState(new DriveStraightForward(this, 0.2, 4930));
                 addState(new TurnRight(this, 0.2, 1580));
                 addState(new RaiseArm(this, 1, 450));
@@ -47,16 +47,11 @@ public class RedBack extends Engine{
                 break;
             case 3:
                 //Left glyph goal
-                addState(new DriveStraightForward(this, 0.2, 4930));
-                addState(new TurnRight(this, 0.2, 1481));
+                addState(new DriveStraightForward(this, 0.2, 3530));
+                addState(new TurnRight(this, 0.2, 513));
                 addState(new RaiseArm(this, 1, 1950));
                 addState(new ExtendArm(this, 0.5, 1290));
                 addState(new LeftGrabber(this, 0.5, 450));
-                addState(new DriveStraightForward(this, -0.5, 516));
-                addState(new RaiseArm(this, 1, 1950));
-                addState(new TurnRight(this, 0.3, 1537));
-                addState(new DriveStraightForward(this,  -0.3, 1000));
-                addState(new DriveStraightForward(this, 0.5, 860));
 
 
 
