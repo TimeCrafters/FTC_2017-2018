2b42c02fd6409389902e6afa07ab713ecb1f785b
Liv007 <Olivia55045@gmail.com>
2017-12-30 12:52:03 -0600

    changes

--- TeamCode/src/main/java/org/timecrafters/Liv/SubEngines/RedBackLeft.java
+++ TeamCode/src/main/java/org/timecrafters/Liv/SubEngines/RedBackLeft.java
@@ -21,7 +21,7 @@ public class RedBackLeft extends SubEngine{
 
     @Override
     public void setProcesses() {
-        addState(new DriveStraightForward(engine, 0.3, 3510));
+        addState(new DriveStraightForward(engine, 0.3, 3511));
         addState(new TurnRight(engine, 0.2, 533));
         addState(new RaiseArm(engine, 1, 450));
         addState(new DriveStraightForward(engine, 0.2, 290));
