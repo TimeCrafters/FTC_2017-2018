479d91dc334b2c3d3379e6a6c72e0c9aac7e60b0
Liv007 <Olivia55045@gmail.com>
2018-01-10 20:58:53 -0600

    changes

--- TeamCode/src/main/java/org/timecrafters/Liv/SubEngines/BlueBackLeft.java
+++ TeamCode/src/main/java/org/timecrafters/Liv/SubEngines/BlueBackLeft.java
@@ -1,13 +1,11 @@
 package org.timecrafters.Liv.SubEngines;
 
-import org.timecrafters.Darcshadowz.State.RightGrabber;
 import org.timecrafters.engine.Engine;
 import org.timecrafters.engine.SubEngine;
 import org.timecrafters.gfp.state.arm.ExtendArm;
 import org.timecrafters.gfp.state.arm.RaiseArm;
 import org.timecrafters.gfp.state.drive.DriveStraightForward;
 import org.timecrafters.gfp.state.drive.TurnLeft;
-import org.timecrafters.gfp.state.drive.TurnRight;
 import org.timecrafters.gfp.state.grabber.LeftGrabber;
 
 /**
@@ -22,7 +20,7 @@ public class BlueBackLeft extends SubEngine{
 
     @Override
     public void setProcesses() {
-        addState(new DriveStraightForward(engine, -0.3, 3500));
+        addState(new DriveStraightForward(engine, -0.3, 3510));
         addState(new TurnLeft(engine, 0.3, 2553));
         addState(new RaiseArm(engine, 1, 650));
         addState(new ExtendArm(engine, 0.5, 1140));
