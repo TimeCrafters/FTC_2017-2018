eecee5e362459b4206151ba1c09e7200ed10cc09
temptingreality <lukebadger777@gmail.com>
2018-02-05 20:33:01 -0600

    added x and b buttons on controller 2 to move grabbers

--- TeamCode/src/main/java/org/timecrafters/gfp/Teleop/Grabbers.java
+++ TeamCode/src/main/java/org/timecrafters/gfp/Teleop/Grabbers.java
@@ -1,5 +1,7 @@
 package org.timecrafters.gfp.Teleop;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 import org.timecrafters.engine.Engine;
 import org.timecrafters.gfp.config.Config;
 
@@ -11,6 +13,9 @@ public class Grabbers extends Config {
 
 
     double power;
+    boolean b=false;
+    boolean x=false;
+    boolean joystick=true;
     public Grabbers(Engine engine,double power){
         super(engine);
         this.power = power;
@@ -40,6 +45,39 @@ public class Grabbers extends Config {
         dcRightGrabber.setPower(engine.gamepad2.right_stick_x);
         dcLeftGrabber.setPower(engine.gamepad2.left_stick_x);
 
+        if (Math.abs(engine.gamepad2.right_stick_x)>=0.1 ||
+            (engine.gamepad2.left_stick_x)>=0.1){
+            joystick=false;
 
+        }else{
+            joystick=true;
+        }
+
+        if(engine.gamepad2.b){
+            if(!b){
+                dcRightGrabber.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+                dcRightGrabber.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                dcRightGrabber.setTargetPosition(dcRightGrabber.getCurrentPosition()+100);
+                dcRightGrabber.setPower(0.3);
+                b=true;
+            }
+        }else{
+
+            b=false;
+            if (joystick){dcRightGrabber.setPower(0.0);}
+        }
+        if(engine.gamepad2.x){
+            if(!x){
+                dcLeftGrabber.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+                dcLeftGrabber.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                dcLeftGrabber.setTargetPosition(dcLeftGrabber.getCurrentPosition()+100);
+                dcLeftGrabber.setPower(-0.3);
+                x=true;
+            }
+        }else{
+
+            x=false;
+            if (joystick){dcLeftGrabber.setPower(0.0);}
+        }
     }
 }
